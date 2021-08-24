b33200b4ba27c5d3b7c75b62d806db8cb8f92292
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2021-08-24 18:06:07 -0300

    sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,44 @@
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import '../styles/room.scss';
+
+import { useParams } from 'react-router-dom';
+
+type RoomParams = {
+    id: string;
+}
+
 export function Room(){
+    //os parametros da minha pagina vao ficar armazenados aqui
+    const params = useParams<RoomParams>();
+
     return (
-        <h1>Sala</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask logo" />
+                    <RoomCode code={params.id} />
+                </div>
+            </header>
+
+            <main>
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea 
+                        placeholder="O que você quer perguntar?"
+                    />
+
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <button>faça seu login</button></span>
+                        <Button type="submit">Enviar pergunta</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     )
 }
