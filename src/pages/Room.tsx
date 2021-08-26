d1328ed75d4841e930549c581800d49fbea1ce22
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2021-08-25 22:28:19 -0300

    questions screen

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -4,12 +4,33 @@ import { RoomCode } from '../components/RoomCode';
 import '../styles/room.scss';
 
 import { useParams } from 'react-router-dom';
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
 
 type RoomParams = {
     id: string;
+}
+
+type FirebaseQuestions = Record<string, {
+    author: {
+        name: string;
+        avatar: string;
+    }
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+}>
+
+type Question = {
+    id: string;
+    author: {
+        name: string;
+        avatar: string;
+    }
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
 }
 
 export function Room(){
@@ -18,6 +39,8 @@ export function Room(){
     const params = useParams<RoomParams>();
 
     const [newQuestion, setNewQuestion] = useState('');
+    const [questions, setQuestions] = useState<Question[]>([]);
+    const [title, setTitle] = useState('');
     const {user} = useAuth();
     const roomId = params.id;
 
@@ -50,6 +73,32 @@ export function Room(){
         setNewQuestion('')
     }
 
+    useEffect(() => {
+        //buscando os daos das perguntas
+        const roomRef = database.ref(`rooms/${roomId}`);
+        
+        //once eu escuto o evento apenas 1x, se voce varias eu colocaria apenas on
+        roomRef.on('value', room => {
+            const databaseRoom = room.val();
+            const firebaseQuestions: FirebaseQuestions = databaseRoom.questions;
+            //retorna umarray com  outro array com chave e valor
+            const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+                return {
+                    id: key,
+                    content: value.content,
+                    author:  value.author,
+                    isHighlighted: value.isHighlighted,
+                    isAnswered: value.isAnswered
+                }
+            })
+
+            setTitle(databaseRoom.title)
+            setQuestions(parsedQuestions)
+        })
+
+        //toda vez que o id da sala mudar eu executo essa funcao novamente
+    }, [roomId]);
+
     return (
         <div id="page-room">
             <header>
@@ -61,8 +110,9 @@ export function Room(){
 
             <main>
                 <div className="room-title">
-                    <h1>Sala React</h1>
-                    <span>4 perguntas</span>
+                    <h1>Sala {title}</h1>
+                    {/* Como nao tenho o else eu coloco so o if que é o && */}
+                    { questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
                 </div>
 
                 <form onSubmit={handleSendQuestion}>
@@ -84,6 +134,8 @@ export function Room(){
                         <Button type="submit" disabled={!user}>Enviar pergunta</Button>
                     </div>
                 </form>
+
+                {JSON.stringify(questions)}
             </main>
         </div>
     )
